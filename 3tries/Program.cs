6310bdfbf1c406c3e579489f6e6c6f3653df3a42
Opilane TTHK <opilane@tthk.ee>
2020-09-24 12:35:39 +0300

    commit

---
+++ 3tries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _3tries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            int userInput;
+
+            int counter = 0;
+
+            while(counter < 3)
+            {
+                if(randomNumber == userInput)
+                {
+                    Console.WriteLine("Congratioilations, you win!");
+                    break;
+                }else
+                {
+                    Console.WriteLine("Try again!");
+                    Console.WriteLine("what is my number?");
+                    userInput = int.Parse(Console.ReadLine());
+
+                    counter++;
+                    Console.WriteLine($"you have guesse {counter}times.");
+                }
+            }    counter = counter + 1;
+            Console.WriteLine($"you have guessed {counter} times");
+        }
+    }
+}
---
+++ GuessingGame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            Console.WriteLine("What is my number?");
+            int userGuess = Int32.Parse(Console.ReadLine());
+            bool correct = false;
+            bool correctGuess = false;
+
+            while(!correctGuess)
+            { 
+                if(randomNumber == userGuess)
+                {
+                    Console.WriteLine("You won!");
+                    correctGuess = true;
+                }else
+                {
+                    if(randomNumber > userGuess)
+                    {
+                        Console.WriteLine("My number is bigger.");
+                        Console.WriteLine("What is my number?");
+                        userGuess = Int32.Parse(Console.ReadLine());
+                    }else
+                    {
+                      Console.WriteLine("my number is smaller?");
+                        Console.WriteLine("What is my number");
+                        userGuess = Int32.Parse(Console.ReadLine());
+                    }
+                    
+                }
+            }
+
+
+        }
+    }
+}
